5c0eb912ba6972ccb01af083a7324b65801df7e9
Matheus Lessa <matheuslessarodrigues@gmail.com>
2018-03-11 20:34:06 -0300

    asdsad

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 extern crate basita;
 
+use std::time::Duration;
+use std::thread;
+
+use basita::sdl2::pixels::Color;
 use basita::specs::{DispatcherBuilder, World};
 
 use basita::sdl::SdlContext;
@@ -9,6 +13,9 @@ use basita::core::components::Transform;
 use basita::renderer::components::Sprite;
 
 pub fn main() {
+	let frames_per_second = 60;
+	let clear_color = Color::RGB(0, 0, 0);
+
 	let sdl_context = SdlContext::new("platform maker", 400, 300);
 	let mut world = World::new();
 
@@ -19,5 +26,28 @@ pub fn main() {
 	world.register::<Transform>();
 	world.register::<Sprite>();
 
-	dispatcher.dispatch(&mut world.res);
+	'main loop {
+		let mut event_pump = sdl_context.event_pump.borrow_mut();
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit { .. } => {
+					break 'main;
+				}
+				_ => (),
+			};
+		}
+
+		let mut canvas = sdl_context.canvas.borrow_mut();
+
+		canvas.set_draw_color(clear_color);
+		canvas.clear();
+
+		dispatcher.dispatch(&mut world.res);
+
+		canvas.present();
+
+		world.maintain();
+
+		thread::sleep(Duration::new(0, 1_000_000_000u32 / frames_per_second));
+	}
 }
