8d1cd3ad53b0a18f8e7178d0c5ed4845b371cd2b
Matheus Lessa <matheus@fastmail.com>
2018-10-30 01:32:43 -0300

    ta dando erro na hora de rodar :(

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+- asset load requests com futures
+- imgui
+- dar uma olhada no renderer?
+- respostas de colisão
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod game;
 use game::*;
 
 pub fn main() {
+	println!("oieee");
 	let frames_per_second = 60;
 
 	let mut sdl_context = SdlContext::new("game", 400, 300);
