7c16fd91647cf6b742fcbe33e0bfd3ccaf7dbd2c
Matheus Lessa <matheus@fastmail.com>
2019-01-04 13:18:06 -0200

    teste de struct que englobaria sdl

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,31 @@ use basita::{core, gui, input, mixer, physics, renderer};
 mod game;
 use crate::game::*;
 
+/* Quase funciona uma struct que engloba tudo (sdl_loader precisa ficar de fora)
+struct Test<'a> {
+	context: SdlContext,
+	loader: &'a SdlLoader,
+	storage: SdlStorage<'a>,
+}
+
+pub fn test() {
+	let sdl_context = SdlContext::new("game", 400, 300, 8);
+	let sdl_loader = SdlLoader::new(&sdl_context); // drop 1
+	let sdl_storage = SdlStorage::default(); // drop 2
+
+	let mut sdl = Test {
+		context: sdl_context,
+		loader: &sdl_loader,
+		storage: sdl_storage,
+	};
+
+	{
+		let mut world = World::new();
+		LazyEvaluations::evaluate(&mut world, &sdl.loader, &mut sdl.storage);
+	}
+}
+*/
+
 pub fn main() {
 	let frames_per_second = 60;
 
